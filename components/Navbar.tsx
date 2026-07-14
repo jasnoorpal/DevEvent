6241bcda1c0357aa358705f0188fcdc6b9b70410
jasnoorpal <jasnoor.official2005@gmail.com>
2026-07-14 21:30:45 +0530

    Implemented MongoDB Atlas, Cloudinary, and dynamic Events and Detailed Event pages.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,23 +2,27 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
-  return (
-    <header>
-      <nav>
-        <Link href="/" className="logo">
-          <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+    return (
+        <header>
+            <nav>
+                <Link href='/' className="logo">
+                    <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
 
-          <p>DevEvent</p>
-        </Link>
+                    <p>DevEvent</p>
+                </Link>
 
-        <ul>
-          <Link href="/">Home</Link>
-          <Link href="/">Events</Link>
-          <Link href="/">Create Event</Link>
-        </ul>
-      </nav>
-    </header>
-  );
-};
+                <ul>
+                    <Link href="/">Home</Link>
+                    <Link href="#" className="text-gray-400 hover:cursor-default">
+                        Events
+                    </Link>
+                    <Link href="#" className="text-gray-400 hover:cursor-default">
+                        Create Event
+                    </Link>
+                </ul>
+            </nav>
+        </header>
+    )
+}
 
-export default Navbar;
+export default Navbar
--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    cacheComponents: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            }
+        ]
+    },
   async rewrites() {
     return [
       {
         source: "/ingest/static/:path*",
         destination: "https://us-assets.i.posthog.com/static/:path*",
-      },
-      {
-        source: "/ingest/array/:path*",
-        destination: "https://us-assets.i.posthog.com/array/:path*",
       },
       {
         source: "/ingest/:path*",
